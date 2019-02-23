4a689cf812f40717c0132d4b3e780e8292527bc8
Justin Shrake <justinshrake@gmail.com>
2019-02-22 17:25:15 -0800

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+extern crate image;
+extern crate serde;
+extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
+
+#[derive(Debug, Serialize)]
+struct ReglTexture {
+    width: u32,
+    height: u32,
+    data: Vec<u8>,
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let path = std::path::Path::new(&args[1]);
+    assert!(path.exists());
+    let img = image::open(&path).expect("open image failed").to_rgba();
+    let (width, height) = img.dimensions();
+    let data: Vec<u8> = img.into_raw();
+    let regl_texture = ReglTexture {
+        width,
+        height,
+        data,
+    };
+    let json = serde_json::to_string(&regl_texture).unwrap();
+    print!("{}", json);
+}
